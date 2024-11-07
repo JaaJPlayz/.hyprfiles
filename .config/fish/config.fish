06f8fd9c24d3b22b5be2ee89012906548b2f40d7
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2024-11-06 23:04:37 -0300

    Feat: Adapting config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    alias cat="bat"
+    alias ls="lsd"
     set fish_greeting
     starship init fish | source
 end
